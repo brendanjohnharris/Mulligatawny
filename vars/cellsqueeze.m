416a5ec012e5d85a766b7bb8b8e055e7816a54f6
BrendanJHarris <brendanjohnharris@gmail.com>
2020-09-18 08:13:39 +1000

    Take non-cell inputs, do nothing

--- vars/cellsqueeze.m
+++ vars/cellsqueeze.m
@@ -3,14 +3,16 @@ function slra = cellsqueeze(slra)
 % 1x1 cell elements.
 % If slra is multidimensional, it will be flattened by concatenating its
 % columns.
-    slra = slra(:);
-    fullinds = ~cellfun(@isempty, slra);
-    slra = slra(fullinds);
-    deepinds = cellfun(@(x) numel(x) == 1 && iscell(x), slra);
-    slra(deepinds) = cellfun(@(x) x{1}, slra(deepinds), 'uniformoutput', 0);
-    
-    while prod(size(slra)) == 1 && iscell(slra)
-    	slra = slra{1};
+    if iscell(slra)
+        slra = slra(:);
+        fullinds = ~cellfun(@isempty, slra);
+        slra = slra(fullinds);
+        deepinds = cellfun(@(x) numel(x) == 1 && iscell(x), slra);
+        slra(deepinds) = cellfun(@(x) x{1}, slra(deepinds), 'uniformoutput', 0);
+
+        while prod(size(slra)) == 1 && iscell(slra)
+            slra = slra{1};
+        end
     end
 end
 
